c133b91dda638bb372049ac1910dc9e015776cd5
Max Arbess <maxarbess1@gmail.com>
2019-02-02 00:56:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var recipes = {}
+var newRecipes = Object.assign({},obj)
 function updateObjectWithKeyAndValue(object,key,value)  {
   return Object.assign({prop: 1, prop2: 2}, Object[key]=value)
 }
@@ -7,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value)  {
   return object
 }
 function deleteFromObjectByKey(object,key)  {
-  return delete object.key
+  return delete newRecipes.key
 }
 function destructivelyDeleteFromObjectByKey(object,key)  {
   return delete object.key
